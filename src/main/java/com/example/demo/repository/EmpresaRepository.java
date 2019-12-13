b8cae11f68f9f18d358f7ff7a01057027a74b97c
anaferreiracodeandgo <anaferreira@codeandgo.com.br>
2019-12-13 11:10:30 -0300

    arquivos iniciais

--- src/main/java/com/example/demo/repository/EmpresaRepository.java
+++ src/main/java/com/example/demo/repository/EmpresaRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class EmpresaRepository{
+import com.example.demo.data.Empresa;
 
+public interface EmpresaRepository extends JpaRepository<Empresa, Long>{
+	
+	Empresa findByCnpj(String cnpj);		
+	
 }
